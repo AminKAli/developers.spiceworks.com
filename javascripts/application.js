96cb91dff0287279c360f6319008f1ffd98f490b
Michael Gerbush <michaelg@spiceworks.com>
2015-04-22 18:11:30 -0500

    Automated commit at 2015-04-22 23:11:30 UTC by middleman-deploy 1.0.0

--- javascripts/application.js
+++ javascripts/application.js
@@ -15518,6 +15518,58 @@ onReady = {
 };
 
 jQuery( document ).ready( onReady.init );
+// >=IE10 no longer supports conditional comments so we have to manually add
+// the 'ie' class to html for that browser
+// See: http://jsfiddle.net/evildonald/jLuF5/
+
+jQuery(function(){
+  var ie10Styles = [
+    'msTouchAction', 'msWrapFlow', 'msWrapMargin',
+    'msWrapThrough', 'msOverflowStyle', 'msScrollChaining',
+    'msScrollLimit', 'msScrollLimitXMin', 'msScrollLimitYMin',
+    'msScrollLimitXMax', 'msScrollLimitYMax', 'msScrollRails',
+    'msScrollSnapPointsX', 'msScrollSnapPointsY', 'msScrollSnapType',
+    'msScrollSnapX', 'msScrollSnapY', 'msScrollTranslation',
+    'msFlexbox', 'msFlex', 'msFlexOrder'];
+
+  var ie11Styles = ['msTextCombineHorizontal'];
+
+  /*
+   * Test all IE only CSS properties
+   */
+  var body = document.body;
+  var style = body.style;
+  var ieVersion = null;
+  var i, property;
+
+  for (i = 0; i < ie10Styles.length; i++) {
+    property = ie10Styles[i];
+
+    if (style[property] != undefined) {
+      ieVersion = "ie10";
+      break; //short circuit the loop
+    }
+  }
+
+  for (i = 0; i < ie11Styles.length; i++) {
+    property = ie11Styles[i];
+
+    if (style[property] != undefined) {
+      ieVersion = "ie11";
+      break; //short circuit the loop
+    }
+  }
+
+  //Update DOM to know about IE-ness
+  if (ieVersion) {
+    jQuery('html').removeClass('no-ie').addClass('ie').addClass(ieVersion);
+  }
+
+  //Legacy support for <= IE9
+  if (document.attachEvent && !jQuery('html').hasClass('ie')) {
+    jQuery('html').removeClass('no-ie').addClass('ie');
+  }
+});
 /*!
  * Ladda
  * http://lab.hakim.se/ladda
@@ -17099,7 +17151,12 @@ jQuery( document ).ready( onReady.init );
       var login = new SW.Login({clientId: 'bfe9764216a6762c790e314923e277fd23064dc2c7f424c5fe930a9379f4bb71'});
       $('.download-dev-edition-button').on('click', function (e) {
         e.preventDefault();
-        login.request('login').then(onLogin);
+        if ($('html').hasClass('ie')) {
+          onLogin();
+        }
+        else {
+          login.request('login').then(onLogin);
+        }
       });
     }
   };
