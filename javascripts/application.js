7bb0dee099bc275bd45c1228f8c5cbc1a3079614
Michael Gerbush <michaelg@spiceworks.com>
2015-04-21 20:14:43 -0500

    Automated commit at 2015-04-22 01:14:42 UTC by middleman-deploy 1.0.0

--- javascripts/application.js
+++ javascripts/application.js
@@ -17085,6 +17085,26 @@ jQuery( document ).ready( onReady.init );
   };
 
 }(jQuery));
+(function($) {
+  'use strict';
+
+  var DOWNLOAD_URL = 'http://download.spiceworks.com/DesktopDevEdition/current/Spiceworks.exe';
+
+  var onLogin = function () {
+    window.location.href = DOWNLOAD_URL;
+  }
+
+  developers.downloads = {
+    init: function () {
+      var login = new SW.Login({clientId: 'bfe9764216a6762c790e314923e277fd23064dc2c7f424c5fe930a9379f4bb71'});
+      $('.download-dev-edition-button').on('click', function (e) {
+        e.preventDefault();
+        login.request('login').then(onLogin);
+      });
+    }
+  };
+
+}(jQuery));
 (function() {
   (function($) {
     return developers.support = {
